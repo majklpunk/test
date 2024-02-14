dcb3cb9e4b468474c2d0375641d133545ddb1d23
majklpunk <142232949+majklpunk@users.noreply.github.com>
2024-02-14 14:34:34 +0100

    commit changes

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+shared_scripts { '@ox_lib/init.lua' }
+
+client_scripts { 'main.js' }
---
+++ main.js
@@ -0,0 +1,3 @@
+const lodash = require('lodash')
+const shuffledArray = lodash.shuffle([1, 2, 3, 4, 5]);
+console.log(shuffledArray);
